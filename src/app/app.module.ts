9793001f2c6e8322954dab9c6e2ed8b7381641d1
Ashutosh Soni <ashutosh.s@beehyv.com>
2023-02-23 12:27:28 +0530

    Updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
